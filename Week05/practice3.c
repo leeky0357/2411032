f02340f217a4e8c1a547fd2969184b4fb630ed2e
leeky0357 <leeky0204@naver.com>
2024-10-02 13:58:33 +0900

    Week05

---
+++ Week05/practice3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	
+	printf("Input the second: ");
+	scanf("%i",&a);
+	
+	printf("The time is %i : %i", a/60, a%60);
+	
+	return 0;
+}
---
+++ Week05/practice4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	
+	
+	printf("Input the year: ");
+	scanf("%i",&a);
+	
+	printf("Is the year %i the leap year?",a);
+	printf("%i", a%4==0 && a%100!=0 || a%400==0);
+		
+	return 0;
+}
